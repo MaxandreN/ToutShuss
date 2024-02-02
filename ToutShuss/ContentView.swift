5f31dddbd7d6000789c7a63dc0f764f37cc0cd9a
Max <Maxandre.neveux@gmail.com>
2024-02-02 16:19:57 +0100

    Initial Commit

---
+++ ToutShuss/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToutShuss
+//
+//  Created by Maxandre Neveux on 02/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToutShuss/ToutShussApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToutShussApp.swift
+//  ToutShuss
+//
+//  Created by Maxandre Neveux on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToutShussApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
